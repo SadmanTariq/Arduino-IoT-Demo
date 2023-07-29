30b0f9185627b7c2fd43caba24382db0f54758bb
Sadman Tariq <sadmantariq@gmail.com>
2023-07-30 08:12:32 +1000

    Rewrite web app using bootstrap.

--- webapp/index.js
+++ webapp/index.js
@@ -1,10 +1,8 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js';
-import { getFunctions, httpsCallable, connectFunctionsEmulator } from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-functions.js';
-
-const DEBUG = false;
+// import { getFunctions, httpsCallable, connectFunctionsEmulator } from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-functions.js';
+import { getDatabase, onValue, update, set, ref, connectDatabaseEmulator } from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-database.js';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCnx3I0MGLDhLIqj-0P5xXDm6vbQgCtqy8",
     authDomain: "arduino-iot-demo-785d8.firebaseapp.com",
     databaseURL: "https://arduino-iot-demo-785d8-default-rtdb.asia-southeast1.firebasedatabase.app",
     projectId: "arduino-iot-demo-785d8",
@@ -15,45 +13,181 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const functions = getFunctions(app);
-
-if (DEBUG)
-    connectFunctionsEmulator(functions, "127.0.0.1", 5001);
-
-const setPin = httpsCallable(functions, 'setPin');
-const resetDb = httpsCallable(functions, 'resetDb');
-const getPins = httpsCallable(functions, 'getPins');
-
-async function togglePin(pin) {
-    return setPin({ pin: pin, value: 1 })
-}
-
-for (let i = 2; i <= 13; i++) {
-    document.getElementById('btn-pin-' + i).addEventListener('click', () => {
-        togglePin(i)
-            .then(updatePins());
+const db = getDatabase(app);
+if (location.hostname === "localhost") {
+    // Point to the RTDB emulator running on localhost.
+    connectDatabaseEmulator(db, "127.0.0.1", 9000);
+    // Reset database.
+    set(ref(db), {
+        "pins": [
+            null,
+            null,
+            true,
+            false,
+            false,
+            false,
+            true,
+            true,
+            true,
+            true,
+            false,
+            true,
+            true
+        ],
+        "sensors": {
+            "altitude": -3.662937,
+            "gas_resistance": 50397,
+            "humidity": 43.838787,
+            "lpg_content": 434,
+            "potentiometer": 357,
+            "pressure": 101370,
+            "temperature": 25.191204
+        }
     });
 }
 
-document.getElementById('btn-reset').addEventListener('click', () => {
-    console.log("reset");
-    resetDb()
-        .then((result) => {
-            console.log(result);
-        })
-        .catch((error) => {
-            console.error(error);
-        });
-    updatePins();
+onValue(ref(db, "sensors"), (snapshot) => {
+    let sensors = snapshot.val();
+    sensors.temperature = sensors.temperature?.toFixed(2);
+    sensors.humidity = sensors.humidity?.toFixed(2);
+    sensors.pressure = (sensors.pressure / 100).toFixed(2);
+    sensors.gas_resistance = (sensors.gas_resistance / 1000).toFixed(2);
+
+    const idSensorMap = {
+        "temp-value": "temperature",
+        "humidity-value": "humidity",
+        "pressure-value": "pressure",
+        "gasresistance-value": "gas_resistance",
+        "lpgcontent-value": "lpg_content"
+    }
+    for (const id in idSensorMap) {
+        if (Object.hasOwnProperty.call(idSensorMap, id)) {
+            const element = idSensorMap[id];
+            document.getElementById(id).innerHTML = sensors[element];
+        }
+    }
 });
 
-async function updatePins() {
-    let pins = await getPins();
-    pins = pins.data;
-    console.log(pins);
-    for (let i = 2; i <= 13; i++) {
-        document.getElementById('pin-' + i + '-status').innerHTML = pins[i].value ? "ON" : "OFF";
+
+let ledState = {
+    led1: false,
+    led2: false,
+    led3: {
+        state: false,
+        color: "red"
+    },
+}
+
+const btnLed1 = document.getElementById("btn-led1");
+const btnLed2 = document.getElementById("btn-led2");
+const btnLed3 = document.getElementById("btn-led3-toggle");
+const btnLed3Red = document.getElementById("btn-led3-red");
+const btnLed3Green = document.getElementById("btn-led3-green");
+const btnLed3Blue = document.getElementById("btn-led3-blue");
+
+const pinMap = {
+    led1: 6,
+    led2: 7,
+    led3Gnd: 10,
+    led3Red: 11,
+    led3Green: 9,
+    led3Blue: 12,
+}
+
+function activateButton(btn) {
+    if (!btn.classList.contains("active")) {
+        btn.classList.add("active");
     }
 }
 
-setInterval(updatePins, 1000);
+function deactivateButton(btn) {
+    if (btn.classList.contains("active")) {
+        btn.classList.remove("active");
+    }
+}
+
+function setButtons(state) {
+    if (state.led1) {
+        activateButton(btnLed1);
+    } else {
+        deactivateButton(btnLed1);
+    }
+
+    if (state.led2) {
+        activateButton(btnLed2);
+    } else {
+        deactivateButton(btnLed2);
+    }
+
+    if (state.led3.state) {
+        activateButton(btnLed3);
+    } else {
+        deactivateButton(btnLed3);
+    }
+
+    if (state.led3.color === "red") {
+        activateButton(btnLed3Red);
+    } else {
+        deactivateButton(btnLed3Red);
+    }
+
+    if (state.led3.color === "green") {
+        activateButton(btnLed3Green);
+    } else {
+        deactivateButton(btnLed3Green);
+    }
+
+    if (state.led3.color === "blue") {
+        activateButton(btnLed3Blue);
+    } else {
+        deactivateButton(btnLed3Blue);
+    }
+}
+
+onValue(ref(db, "pins"), (snapshot) => {
+    let pins = snapshot.val();
+    ledState.led1 = pins[pinMap.led1];
+    ledState.led2 = pins[pinMap.led2];
+    ledState.led3 = {
+        state: !pins[pinMap.led3Gnd],
+        color: pins[pinMap.led3Red] ? "red" : pins[pinMap.led3Green] ? "green" : "blue"
+    };
+
+    setButtons(ledState);
+});
+
+btnLed1.addEventListener("click", () => {
+    set(ref(db, "pins/" + pinMap.led1), !ledState.led1);
+});
+
+btnLed2.addEventListener("click", () => {
+    set(ref(db, "pins/" + pinMap.led2), !ledState.led2);
+});
+
+btnLed3.addEventListener("click", () => {
+    set(ref(db, "pins/" + pinMap.led3Gnd), ledState.led3.state);
+});
+
+btnLed3Red.addEventListener("click", () => {
+    update(ref(db, "pins"), {
+        [pinMap.led3Red]: true,
+        [pinMap.led3Green]: false,
+        [pinMap.led3Blue]: false,
+    });
+});
+
+btnLed3Blue.addEventListener("click", () => {
+    update(ref(db, "pins"), {
+        [pinMap.led3Red]: false,
+        [pinMap.led3Green]: false,
+        [pinMap.led3Blue]: true,
+    });
+});
+
+btnLed3Green.addEventListener("click", () => {
+    update(ref(db, "pins/"), {
+        [pinMap.led3Red]: false,
+        [pinMap.led3Green]: true,
+        [pinMap.led3Blue]: true,
+    });
+});
